774546d31c39eda7f2ea4309ce6beb89a9e21dbd
MiguelDiazM <mdm947531@gmail.com>
2024-03-14 16:02:41 +0100

    Dia 14/03/2024

---
+++ Persona/src/Persona/Persona.java
@@ -0,0 +1,105 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellidos;
+
+    /**
+     * 
+     */
+    private int edad;
+
+    /**
+     * @param dni 
+     * @param nombre 
+     * @param apellidos 
+     * @param edad
+     */
+    public Persona(String dni, String nombre, String apellidos, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String GetDni() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param dni
+     */
+    public void SetDni(String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String GetNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param nombre
+     */
+    public void SetNombre(String nombre) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String GetApellidos() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param apellidos
+     */
+    public void SetApellidos(String apellidos) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int GetEdad() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param int
+     */
+    public void SetEdad(edad int) {
+        // TODO implement here
+    }
+
+}
